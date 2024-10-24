e39bfa33d1052dc6a6e088c2e3b4a10d004d98f6
markovolimango <maremare005@gmail.com>
2024-10-24 22:04:29 +0200

    tidied up

--- main.cpp
+++ main.cpp
@@ -16,6 +16,29 @@ int main() {
     out = to_string(obj);
     std::cout << out << std::endl;
 
-    std::cout << std::get<int>(std::get<std::shared_ptr<json::Object> >(obj.data["a"][0].value)->data["b"][1].value);
+    //std::cout << std::get<int>(std::get<std::shared_ptr<json::Object> >(obj.data["a"][0].value)->data["b"][1].value);
     std::string input = "a.b[1]";
+    /*json::Element elem;
+    elem.value = std::make_shared<json::Object>(obj);
+    i = 0;
+    while (i < input.size()) {
+        std::cout << json::to_string(elem) << std::endl;
+        std::string name = "";
+        while (input[i] != '.' && input[i] != '[' && i < input.size()) {
+            name += input[i];
+            i++;
+        }
+        if (input[i] == '.') {
+            elem = std::get<std::shared_ptr<json::Object> >(elem.value)->data[name][0];
+            i++;
+        } else if (input[i] == '[') {
+            i++;
+            int pos = s[i] - '0';
+            elem = std::get<std::shared_ptr<json::Object> >(elem.value)->data[name][pos];
+            i++;
+        } else {
+            i++;
+        }
+    }
+    std::cout << json::to_string(elem);*/
 }
