556dc2605096d8dde71e018bd9d8f0361ef7399e
markovolimango <maremare005@gmail.com>
2024-10-31 17:46:35 +0100

    renamed errors

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ int main() {
     int i = 0;
     try {
         root = json::parseObject(text, i);
-    } catch (const json::Error &erke) {
-        std::cerr << erke.what() << std::endl;
+    } catch (const json::Error &error) {
+        std::cerr << error.what() << std::endl;
         return 1;
     }
 
@@ -30,8 +30,8 @@ int main() {
     i = 0;
     try {
         res = json::readElement(root, input, i);
-    } catch (const json::Error &erke) {
-        std::cerr << erke.what() << std::endl;
+    } catch (const json::Error &error) {
+        std::cerr << error.what() << std::endl;
         return 1;
     }
     std::cout << json::to_string(res) << std::endl;
