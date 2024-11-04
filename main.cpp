ce6305e923435215b5763f2d8713a965419a1d7a
markovolimango <maremare005@gmail.com>
2024-11-04 14:00:12 +0100

    tidied up the main func

--- main.cpp
+++ main.cpp
@@ -1,58 +1,54 @@
+#include "json.h"
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include "json.h"
 
 int main() {
-    std::string filename, text, input;
-    std::stringstream buffer;
-    json::Object root;
-    json::Element res;
-    std::ifstream file;
+  std::string filename, text, input;
+  std::stringstream buffer;
+  json::Object root;
+  json::Element res;
+  std::ifstream file;
 
-    //std::cin >> filename;
-    filename = "text.json";
-    file.open(filename);
-    if (!file) {
-        std::cerr << "Error opening file " << filename << "." << std::endl;
-        return 1;
-    }
-    buffer << file.rdbuf();
-    text = buffer.str();
-    int i = 0;
+  std::cin >> filename;
+  file.open(filename);
+  if (!file) {
+    std::cerr << "Error opening file " << filename << "." << std::endl;
+    return 1;
+  }
+  buffer << file.rdbuf();
+  text = buffer.str();
+  int i = 0;
+  try {
+    root = json::parseObject(text, i);
+  } catch (const json::Error &error) {
+    std::cerr << error.what() << std::endl;
+    return 1;
+  }
+
+  std::cin >> input;
+
+  i = 0;
+  size_t op = json::findOperation(input);
+  if (op != std::string::npos) {
+    float x;
     try {
-        root = json::parseObject(text, i);
+      x = json::performOperation(root, input, op);
     } catch (const json::Error &error) {
-        std::cerr << error.what() << std::endl;
-        return 1;
+      std::cerr << error.what() << std::endl;
+      return 1;
     }
-
-    std::cin >> input;
-
-    i = 0;
-    size_t op = json::findOperation(input);
-    if (op != std::string::npos) {
-        float x;
-        try {
-            x = json::performOperation(root, input, op);
-        } catch (const json::Error &error) {
-            std::cerr << error.what() << std::endl;
-            return 1;
-        }
-        std::cout << json::to_string(x) << std::endl;
-        return 0;
-    }
-
-    try {
-        res = json::readElement(root, input, i);
-    } catch (const json::Error &error) {
-        std::cerr << error.what() << std::endl;
-        return 1;
-    }
-    std::cout << json::to_string(res) << std::endl;
-
+    std::cout << json::to_string(x) << std::endl;
     return 0;
+  }
+
+  try {
+    res = json::readElement(root, input, i);
+  } catch (const json::Error &error) {
+    std::cerr << error.what() << std::endl;
+    return 1;
+  }
+  std::cout << json::to_string(res) << std::endl;
+
+  return 0;
 }
-
-
-
